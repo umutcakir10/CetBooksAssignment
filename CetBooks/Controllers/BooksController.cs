1e7a76fa03c3dddcb2f6a8ce39fbdb1d1bdcf8e2
umutcakir10 <ucakir81@gmail.com>
2024-03-06 14:44:20 +0300

    Proje dosyası ekle.

---
+++ CetBooks/Controllers/BooksController.cs
@@ -0,0 +1,15 @@
+﻿using CetBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CetBooks.Controllers
+{
+    public class BooksController
+    {
+        public IActionResult Index()
+        {
+            FakeDB fakeDB = new FakeDB();
+            var allbo
+        }
+    }
+}
